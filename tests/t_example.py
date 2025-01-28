4878a3855d09f16a324de43723261baefe6da8e9
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-01-28 05:35:04 +0100

    General: Moved example.py into tests

--- tests/t_example.py
+++ tests/t_example.py
@@ -1,7 +1,16 @@
 from holytools.devtools import Unittest
 
-from example import ExampleClass
 
+class ExampleClass:
+    def __init__(self, a : float, b : float):
+        self.a : float = a
+        self.b : float = b
+
+    def sum(self) -> float:
+        return self.a + self.b
+
+    def multiply(self) -> float:
+        return self.a * self.b
 
 class TestExample(Unittest):
     def setUp(self):
