f38288ef0265b02c507b228389bee4bcacff2b30
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-03-01 11:41:20 +0100

    General: Updated mismatched pkg_to_module map

--- devops/dep.py
+++ devops/dep.py
@@ -267,7 +267,7 @@ def cli(
 
     default_pkg_to_module_map = {'pillow' : 'PIL',
                                  'beautifulsoup4' : 'bs4',
-                                 'progressbar' : 'progressbar2'}
+                                 'progressbar2' : 'progressbar'}
     for pkg in default_pkg_to_module_map:
         module = default_pkg_to_module_map[pkg]
         package_module_name_map[pkg] = (module,)
